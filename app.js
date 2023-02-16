39eb1b476d1e9b0f804c2673b56a84da6abc56ab
hashus37 <66906497+hashus37@users.noreply.github.com>
2023-02-16 20:20:25 +0300

    emoji-rating
     emoji rating

---
+++ app.js
@@ -0,0 +1,27 @@
+const startsEl=document.querySelectorAll(".fa-star");
+const emojisEl = document.querySelectorAll(".far");
+const colorsArray=["red","orange","lightblue","lightgreen","green"]
+
+updateRating(0);
+
+startsEl.forEach((starEl, index) =>{
+    starEl.addEventListener("click",() => {
+        updateRating(index)
+        
+    })
+});
+
+function updateRating(index){
+   startsEl.forEach((starEl, idx) => {
+    if(idx < index+1){
+        starEl.classList.add("active");
+    } else {
+        starEl.classList.remove("active");
+    }
+   });
+
+   emojisEl.forEach(emojiEl => {
+    emojiEl.style.transform = `translateX(-${index*50}px)`;
+    emojiEl.style.color = colorsArray[index]
+   });
+}
